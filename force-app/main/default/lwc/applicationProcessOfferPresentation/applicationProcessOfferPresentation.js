b76a92804eb147388e483be601167e29cae2a72e
andrew-j-francis <andrewfrancis@chief.com>
2024-11-11 14:31:28 -0500

    final commit

--- force-app/main/default/lwc/applicationProcessOfferPresentation/applicationProcessOfferPresentation.js
+++ force-app/main/default/lwc/applicationProcessOfferPresentation/applicationProcessOfferPresentation.js
@@ -17,12 +17,35 @@ export default class ApplicationProcessOfferPresentation extends LightningElemen
     ];
 
     @api loanData = "";
-
+    @api application;
     tableData = "";
+    isApproved = true;
 
     connectedCallback(){
         this.tableData = Json.parse(this.loanData);
+
+        console.log(this.loanData);
+
+        calculateApproval();
     }
+
+    calculateApproval(){
+        let totalLoanAmount = this.tableData.termInMonths * this.tableData.monthlyPayment;
+
+
+        if(totalLoanAmount < 10000 || totalLoanAmount > 50000){
+            this.isApproved = false;
+        }
+
+        if (application.Total_Lines_Of_Credit__c < 10){
+            this.tableData.termInMonths = 36;
+            this.tableData.interestRatePercentage = 10;
+        }
+    }
+
+
+
+
 
 
 
